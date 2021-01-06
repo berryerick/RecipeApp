b6f7d66f83a089d25efeee95b3af0f2804cda956
berryerick <berryerick@gmail.com>
2021-01-05 21:02:35 -0800

    components connected

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Recipe from './assets/components/Recipe';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Recipe></Recipe>
+      <Recipe></Recipe>
+      <Recipe></Recipe>
+      <Recipe></Recipe>
+
     </View>
   );
 }
---
+++ assets/components/Recipe.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, TextInput, View } from 'react-native';
+
+const Recipe  = () => {
+    return (
+    <View>
+        <Text>Recipe block</Text>
+        <View>
+            <Text>Ingredient</Text>
+            <Text>Ingredient</Text>
+        </View>
+        <View>
+            <Text>instructions</Text>
+        </View>
+    </View>
+    );
+  }
+
+export default Recipe;
